2269dfe33fa6aaef10ba87412f353fb475a1c13a
alee0510 <ali.muksin0510@gmail.com>
2020-03-09 14:26:14 +0700

    set-up qr scanner

--- src/screens/qrSanner.js
+++ src/screens/qrSanner.js
@@ -1,22 +1,55 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
+import QRCodeScanner from 'react-native-qrcode-scanner'
+import { RNCamera as Camera } from 'react-native-camera'
 
 class QrScanner extends React.Component {
+    onSuccess = (e) => {
+    // Linking
+    //   .openURL(e.data)
+    //   .catch(err => console.error('An error occured', err));
+        console.log(e.data)
+    }
     render () {
         return (
-            <View style = {styles.container}>
-                <Text style = {{ fontSize : 24 }}>QrScanner</Text>
-            </View>
+            <QRCodeScanner
+                onRead={this.onSuccess}
+                flashMode={Camera.Constants.FlashMode.auto}   
+                reactivate = {true}
+                reactivateTimeout = {5000}   
+                topContent={
+                    <Text style={styles.centerText}>
+                        Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                    </Text>
+                }
+                bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                }
+            />
         )
     }
 }
 
-const styles = StyleSheet.create ({
-    container : {
-        flex : 1,
-        justifyContent : 'center',
-        alignItems : 'center'
-    }
+const styles = StyleSheet.create({
+    centerText: {
+      flex: 1,
+      fontSize: 18,
+      padding: 32,
+      color: '#777',
+    },
+    textBold: {
+      fontWeight: '500',
+      color: '#000',
+    },
+    buttonText: {
+      fontSize: 21,
+      color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+      padding: 16,
+    },
 })
 
 export default QrScanner
