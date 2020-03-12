747d4313f92e6055a386a7b39ea3a926de1756d5
alee0510 <ali.muksin0510@gmail.com>
2020-03-12 22:32:47 +0700

    initialize top up page

---
+++ src/screens/topUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Button, Overlay } from 'react-native-animatable'
+
+class TopUp extends React.Component {
+    render () {
+        return (
+            <View>
+                <Text></Text>
+            </View>
+        )
+    }
+}
+
+export default TopUp
