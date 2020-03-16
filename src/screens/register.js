b57c3a281f313e9c8fedcca6921f96e524e8df31
alee0510 <ali.muksin0510@gmail.com>
2020-03-16 23:44:33 +0700

    bugs fixed

--- src/screens/register.js
+++ src/screens/register.js
@@ -145,7 +145,7 @@ const styles = StyleSheet.create ({
     buttonStyle : {
         borderRadius : 50,
         backgroundColor : colors.main.flatRed,
-        fontSize : 20
+        // fontSize : 20
     },
 })
 
